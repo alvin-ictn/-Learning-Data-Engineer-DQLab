feefc3f1ad8eb37e832dfcdbe64526779173af9c
Alvin Mantovani <alvin.ictn@gmail.com>
2021-03-25 05:43:34 +0700

    add files

---
+++ Python/Python for Data Professional Beginner - Part 1/3. Python Operators/14. Tugas Praktek/source.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 23 21:54:27 2021
+
+@author: Alvin
+
+Tugas:
+Dengan cara yang diajarkan Senja, aku akan membuat potongan kode diatas menjadi lebih simpel.
+ 
+"""
+
+sepatu = { "nama" : "Sepatu Niko", "harga": 150000, "diskon": 30000 }
+baju = { "nama" : "Baju Unikloh", "harga": 80000, "diskon": 8000 }
+celana = { "nama" : "Celana Lepis", "harga": 200000, "diskon": 60000 }
+harga_sepatu = sepatu["harga"] - sepatu["diskon"]
+harga_baju = baju["harga"] - baju["diskon"]
+harga_celana = celana["harga"] - celana["diskon"]
+total_harga = (harga_sepatu + harga_baju + harga_celana) * 1.1 
+print(total_harga)
