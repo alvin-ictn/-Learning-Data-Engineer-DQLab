b2ea9937bba1549a26e2fa9e6cb47cf5c4c63d72
Alvin Mantovani <alvin.ictn@gmail.com>
2021-03-25 06:01:12 +0700

    push

---
+++ Python/Python for Data Professional Beginner - Part 1/4. Pythons Conditioning & Looping/2. Python Conditioning for Decision – Part 2/source.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 25 05:48:58 2021
+
+@author: Alvin
+
+Tugas:
+Praktekkan kode berikut di live code editor pada baris setelah kode sebelumnya yang telah dibuat.
+"""
+
+# Statement if
+x = 4
+if x % 2 == 0: # jika sisa bagi x dengan 2 sama dengan 0
+    print("x habis dibagi dua") # statemen aksi lebih menjorok ke dalam
+# Statement if ... elif ... else
+x = 7
+if x % 2 == 0: # jika sisa bagi x dengan 2 sama dengan 0
+    print("x habis dibagi dua")
+elif x % 3 == 0: # jika sisa bagi x dengan 3 sama dengan 0
+    print("x habis dibagi tiga")
+elif x % 5 == 0: # jika sisa bagi x dengan 5 sama dengan 0
+    print("x habis dibagi lima")
+else:
+    print("x tidak habis dibagi dua, tiga ataupun lima")
