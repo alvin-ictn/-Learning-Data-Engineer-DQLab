22a78f1bee35e6257b9261f65ab9a28fb862f4fe
Alvin Mantovani <alvin.ictn@gmail.com>
2021-03-24 20:43:39 +0700

    add files

---
+++ Python/Python for Data Professional Beginner - Part 1/2. Python Variables & Data Types/13. Tugas Praktek/source.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 23 21:54:27 2021
+
+@author: Alvin
+
+Tugas Praktek
+Setelah berhasil merepresentasikan setiap barang ke dalam tipe data dictionary dengan variabel nama, harga, dan diskon, langkahku selanjutnya adalah: mendeklarasikan list dengan nama daftar_belanja yang berisi data sepatu, baju, dan celana.
+"""
+
+sepatu = {"nama": "Sepatu Niko", "harga": 150000, "diskon": 30000} 
+baju = {"nama": "Baju Unikloh", "harga": 80000, "diskon": 8000} 
+celana = {"nama": "Celana Lepis", "harga": 200000, "diskon": 60000} 
+daftar_belanja = [sepatu, baju, celana]
