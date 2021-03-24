42094f700e525fb7640cd52da97f14d5f1308457
Alvin Mantovani <alvin.ictn@gmail.com>
2021-03-24 19:41:38 +0700

    add files

---
+++ Python/Python for Data Professional Beginner - Part 1/2. Python Variables & Data Types/12. Tugas Praktek/source.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 23 21:54:27 2021
+
+@author: Alvin
+
+Tugas Praktek
+Ternyata seru juga! Aku jadi berniat mengembangkan program kalkulator potongan harga ini untuk beberapa barang sekaligus agar lebih fungsional. Kalau begitu aku perlu rapikan dulu informasi setiap barang di tabel ini.
+
+“Lalu, aku tinggal merepresentasikan semuanya ke tipe data dictionary, dengan begitu akan lebih mudah mengolahnya,” gumamku.
+Aku pun mulai mendeklarasikan variabel sepatu, baju, dan celana untuk menampung informasi barang ke dalam live code editor.
+
+ 
+
+|--Nama Barang--|--Harga--|--Diskon--|
+|  Sepatu Niko  | 150000  |  30000   |
+|  Baju Unikloh |  80000  |   8000   |
+|  Celana Lepis |  200000 |   60000  |
+---------------------------------------
+"""
+
+sepatu = { "nama": "Sepatu Niko", "Harga": 150000, "diskon": 30000 }
+baju = {"nama": "Baju Unikloh", "Harga": 80000, "diskon": 8000}
+celana = {"nama": "Celana Lepis", "Harga": 200000, "diskon": 60000}
