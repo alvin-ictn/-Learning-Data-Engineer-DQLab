ea93cfba6365333ef45a5256021a10927c54f25b
Alvin Mantovani <alvin.ictn@gmail.com>
2021-03-25 06:13:23 +0700

    push source code

---
+++ Python/Python for Data Professional Beginner - Part 1/4. Pythons Conditioning & Looping/8. Python while loops – Part 2/source.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 25 05:48:58 2021
+
+@author: Alvin
+
+Tugas:
+Praktekkan potongan kode berikut pada live code editor.
+Output yang akan dihasilkan adalah -1
+
+
+
+ 
+
+
+"""
+
+tagihan = [50000, 75000, -150000, 125000, 300000, -50000, 200000]
+i = 0
+jumlah_tagihan = len(tagihan)
+total_tagihan = 0
+while i < jumlah_tagihan:
+    # jika terdapat tagihan ke-i yang bernilai minus (di bawah nol),
+    # pengulangan akan dihentikan
+    if tagihan[i] < 0:
+        total_tagihan = -1
+        print("terdapat angka minus dalam tagihan, perhitungan dihentikan!")
+        break
+    total_tagihan += tagihan[i]
+    i += 1
+print(total_tagihan)
