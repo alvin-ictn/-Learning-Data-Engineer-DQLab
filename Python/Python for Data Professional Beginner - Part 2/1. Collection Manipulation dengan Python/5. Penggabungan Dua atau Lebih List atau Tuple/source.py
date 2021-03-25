9aa1cd701a31ded38bed91d9c1344cf01c78fc70
Alvin Mantovani <alvin.ictn@gmail.com>
2021-03-25 17:26:27 +0700

    add sources

---
+++ Python/Python for Data Professional Beginner - Part 2/1. Collection Manipulation dengan Python/5. Penggabungan Dua atau Lebih List atau Tuple/source.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 25 17:19:58 2021
+
+@author: Alvin
+
+Penggabungan Dua atau Lebih List atau Tuple
+Selain dapat melakukan pemotongan terhadap tipe data list/ tuple, aku juga dapat menggabungkan isi dari suatu list (ataupun tuple) dengan list lainnya (dengan tuple lainnya) dengan menggunakan operator penambahan (+). 
+
+Tugas:
+Aku menggunakan potongan kode yang dicontohkan dengan live code editor:
+
+
+
+Akan menghasilkan output:
+
+"""
+
+list_makanan = ['Gado-gado', 'Ayam Goreng', 'Rendang']
+list_minuman = ['Es Teh', 'Es Jeruk', 'Es Campur']
+list_menu = list_makanan + list_minuman
+print(list_menu)
