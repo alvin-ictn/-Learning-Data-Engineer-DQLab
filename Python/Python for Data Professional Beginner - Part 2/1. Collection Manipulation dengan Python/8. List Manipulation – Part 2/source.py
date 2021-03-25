f16db33cba8aae0102ff5c42aad8d5d2936192d0
Alvin Mantovani <alvin.ictn@gmail.com>
2021-03-25 19:09:40 +0700

    add sources

---
+++ Python/Python for Data Professional Beginner - Part 2/1. Collection Manipulation dengan Python/8. List Manipulation – Part 2/source.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 25 17:19:58 2021
+
+@author: Alvin
+
+List Manipulation – Part 2
+Selanjutnya aku akan mempelajari fitur index(), insert(), pop(), remove(), reverse(), dan sort() untuk melakukan manipulasi.
+
+Tugas:
+Ketikkanlah kode yang diberikan pada contoh penggunaan di live code editor.
+"""
+
+  
+# Fitur .index()
+print(">>> Fitur .index()")
+list_score = ['Budi','Sud','Budi','Budi','Budi','Sud','Sud']
+score_pertama_sud = list_score.index('Sud') + 1
+print(score_pertama_sud) # akan menampilkan output 2
+# Fitur .insert()
+print(">>> Fitur .insert()")
+list_score = ['Budi','Sud','Budi','Budi','Sud']
+list_score.insert(3, 'Sud')
+print(list_score)
+# Fitur .pop()
+print(">>> Fitur .pop()")
+list_menu = ['Gado-gado', 'Ayam Goreng', 'Rendang']
+list_menu.pop(1)
+print(list_menu)
+# Fitur .remove()
+print(">>> Fitur .remove()")
+list_menu = ['Gado-gado', 'Ayam Goreng', 'Rendang', 'Ketoprak']
+list_menu.remove('Rendang')
+print(list_menu)
+# Fitur .reverse()
+print(">>> Fitur .reverse()")
+list_menu = ['Gado-gado', 'Ayam Goreng', 'Rendang', 'Ketoprak']
+list_menu.reverse()
+print(list_menu)
+# Fitur .sort()
+print(">>> Fitur .sort()")
+list_menu = ['Gado-gado', 'Ayam Goreng', 'Rendang', 'Ketoprak']
+list_menu.sort() # Default: Ascending
+print(list_menu) 
+list_menu.sort(reverse=True)# Descending
+print(list_menu) 
